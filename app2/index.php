74bb08649c222a9aa33bfc57ea0b136760cdc825
Eduardo Pedroni <edusouzapedroni@gmail.com>
2025-08-25 08:39:46 -0300

    Create index.php

---
+++ app2/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Contato</title>
+</head>
+<body>
+    <h1>Fale Conosco</h1>
+
+    <form method="post" action="">
+      <label for="nome">Nome:</label>label><br>
+      <input type="text" name="nome" id="nome" required><br><br>
+
+      <label for="nome">Email:</label>label><br>
+      <input type="email" name="email" id="email" required><br><br>
+
+      <label for="nome">Mensagem:</label>label><br>
+      <textarea name="mensagem" id="mensagem" rows="5" required></textarea><br><br>
+
+      <input type="submit" value="Enviar">
+
+      <?php
+      if ($_SERVER[]
+</body>
+</html>
