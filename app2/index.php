ff5a244fd1270a0977e3723cdd880554dc1d2573
Eduardo Pedroni <edusouzapedroni@gmail.com>
2025-09-15 07:54:31 -0300

    Update index.php

--- app2/index.php
+++ app2/index.php
@@ -9,19 +9,32 @@
 <body>
     <h1>Fale Conosco</h1>
 
-    <form method="post" action="">
-      <label for="nome">Nome:</label>label><br>
-      <input type="text" name="nome" id="nome" required><br><br>
+    <form method="POST">
+        <label for="nome">Nome:</label><br>
+        <input type="text" name="nome" id="nome" required><br><br>
 
-      <label for="nome">Email:</label>label><br>
-      <input type="email" name="email" id="email" required><br><br>
+        <label for="email">Email:</label><br>
+        <input type="email" name="email" id="email" required><br><br>
 
-      <label for="nome">Mensagem:</label>label><br>
-      <textarea name="mensagem" id="mensagem" rows="5" required></textarea><br><br>
+        <label for="mensagem">Mensagem:</label><br>
+        <textarea name="mensagem" id="mensagem" rows="t"></textarea><br><br>
 
-      <input type="submit" value="Enviar">
+        <input type="submit" value="Enviar">
+    </form>
 
-      <?php
-      if ($_SERVER[]
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            //Coleta os dados do formulário
+            $nome = htmlspecialchars($_POST["nome"]);
+            $email = htmlspecialchars($_POST["email"]);
+            $mensagem = htmlspecialchars($_POST["mensagem"]);
+
+            //Exibe os dados (em uma aplicação real, voc~e poderia salvar ou enviar por email)
+            echo "<h2>Dados Recebidos:</h2>";
+            echo "<p><strong>Nome:</strong> $nome</p>";
+            echo "<p><strong>Email:</strong> $email</p>";
+            echo "<p><strong>Mensagem:</strong> $mensagem</p>";
+        }
+    ?>
 </body>
 </html>
